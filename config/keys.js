036fcf1c40990d5f9adb19416d39164dbe84d134
chaofan85 <fanchao05@gmail.com>
2019-04-02 19:17:19 -0400

    added jwt to get user token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb://chao:chao1985@ds245240.mlab.com:45240/yarg-connector"
+  mongoURI: "mongodb://chao:chao1985@ds245240.mlab.com:45240/yarg-connector",
+  secretKey: "secret"
 };
