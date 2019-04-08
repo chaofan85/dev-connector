34dfcfc4b6280fed37dece71044bcb593ff6b770
chaofan85 <fanchao05@gmail.com>
2019-04-07 23:06:26 -0400

    updated profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Profile Model
+const Profile = require("../../models/Profile");
+// Load User Profile
+const User = require("../../models/User");
+
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
