ccdadd6e70be7df45819fc1259966edcc6256708
chaofan85 <fanchao05@gmail.com>
2019-04-12 20:25:38 -0400

    updated profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,4 +13,24 @@ const User = require("../../models/User");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        errors.noprofile = "There is no profile for this user";
+        if (!profile) {
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+);
+
 module.exports = router;
