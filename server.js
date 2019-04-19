2aaf0ec12bc73f51ada79aa4294715c01ceb9824
chaofan85 <fanchao05@gmail.com>
2019-04-18 23:13:30 -0400

    updated

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use Routes
 app.use("/api/users", users);
