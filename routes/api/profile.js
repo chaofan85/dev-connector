37a76eae4a21719c1bdaf908ed5fea1d46bb68bc
chaofan85 <fanchao05@gmail.com>
2019-08-15 17:18:44 -0400

    updated profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -101,7 +101,7 @@ router.post(
       // Create
       profile = new Profile(profileFields);
 
-      await Profile.save();
+      await profile.save();
       res.json(profile);
     } catch (err) {
       console.error(err.message);
@@ -109,5 +109,37 @@ router.post(
     }
   }
 );
+
+// @route     POST api/profile
+// @desc      Get all profiles
+// @access    Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route     POST api/profile/user/:user_id
+// @desc      Get profile by user ID
+// @access    Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
