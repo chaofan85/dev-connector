9805b94b210181bb839d8c54dd33b53ec8fff40c
chaofan85 <fanchao05@gmail.com>
2019-08-23 09:43:47 -0400

    ready for heroku

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function(req, res, next) {
-  // Get toekn from the header
+  // Get token from the header
   const token = req.header("x-auth-token");
 
   // Check if not token
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 // @route     GET api/profile/me
 // @desc      Get current user's profile
@@ -69,6 +70,7 @@ router.post(
     // Build profile object
     const profileFields = {};
     profileFields.user = req.user.id;
+
     if (company) profileFields.company = company;
     if (website) profileFields.website = website;
     if (location) profileFields.location = location;
@@ -112,7 +114,7 @@ router.post(
   }
 );
 
-// @route     POST api/profile
+// @route     GET api/profile
 // @desc      Get all profiles
 // @access    Public
 router.get("/", async (req, res) => {
@@ -150,6 +152,7 @@ router.get("/user/:user_id", async (req, res) => {
 router.delete("/", auth, async (req, res) => {
   try {
     // @todo - remove users posts
+    await Post.deleteMany({ user: req.user.id });
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
